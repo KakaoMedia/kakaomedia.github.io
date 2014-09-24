daa652e525d22b5c7161499aaed73a92182f4136
Jose Baltodano <jsbaltodano@gmail.com>
2014-09-24 11:34:13 -0600

    testing continue

--- js/script.js
+++ js/script.js
@@ -238,9 +238,9 @@ function handle(delta) {
 }
 
 (function(){
-  var container_original_top =  $('#front_page > .container').css( 'top')
-  $('#front_page, header').css('min-height', $(window).height() + 'px');
-  $('#front_page, header').css('max-height', $(window).height() + 'px');
+  //var container_original_top =  $('#front_page > .container').css( 'top')
+  //$('#front_page, header').css('min-height', $(window).height() + 'px');
+  //$('#front_page, header').css('max-height', $(window).height() + 'px');
   $('#front_page > .container').css( 'top', parseInt(($(window).height() - $('#front_page > .container > div').height()) / 2, 10).toString() + "px");
   // if( $('#front_page > .container').offset().top >= $(window).height() ) {
   //   $('#front_page > .container').css( 'top', 0 );
